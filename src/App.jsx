cd39f7a8ad5ccb9b54cc398c3ddc2acc9dd75e68
bhanuprakashsfdc <bhanusunny7@gmail.com>
2024-05-24 00:02:57 +0530

    pushinng basic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Timer from './components/Timer';
+import Tasks from './components/Tasks';
+import Settings from './components/Settings';
+import Profile from './components/Profile';
+import Header from './components/Header';
+
 import { WEBSITE_NAME } from './constants/constants';
-import './App.css'
 
 function App() {
-
   return (
-    <>
-     <p> {WEBSITE_NAME}</p>
-      <p> I am easy pomodoro </p>
-    </>
-  )
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/timer" element={<Timer />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
