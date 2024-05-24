36ff12cf8295a17601e7caa39565708de33186f4
bhanuprakashsfdc <bhanusunny7@gmail.com>
2024-05-24 21:32:47 +0530

    fixing image issues

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -28,7 +28,7 @@ function ForgotPassword() {
     <div className="min-h-screen flex items-center justify-center bg-red-700">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center">
         <div className="mb-8">
-          <img src="/public/assets/img/glogo.png" alt="Logo" className="mx-auto mb-4" /> {/* Replace with your logo path */}
+          <img src="/assets/img/glogo.png" alt="Logo" className="mx-auto mb-4" /> {/* Replace with your logo path */}
           <h1 className="text-2xl font-bold text-red-700">{ WEBSITE_NAME }</h1>
           <p className="text-gray-700">Reset Password</p>
         </div>
